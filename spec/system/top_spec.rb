918b8f6fa3f588f80270391b3156a1c74c3eea99
koharakouki <ko.kohara02@gmail.com>
2020-04-12 20:30:17 +0900

    トップページの統合テスト追加

--- spec/system/top_spec.rb
+++ spec/system/top_spec.rb
@@ -19,3 +19,38 @@ describe 'トップページのテスト' do
         expect(page).to have_link signup_link, href: new_user_registration_path
       end
     end
+
+    context 'ログインしている場合の挙動を確認' do
+      before do
+        visit new_user_session_path
+        fill_in 'user[name]', with: user.name
+        fill_in 'user[password', with: user.password
+        click_button 'Log in'
+        visit root_path
+      end
+      it 'Log inリンクをクリックしたらユーザー詳細画面へ遷移する' do
+        login_link = find_all('a')[4].native.inner_text
+        click_link login_link
+        expect(page).to have_content 'User info'
+      end
+      it 'Sign upリンクをクリックしたらユーザー詳細画面へ遷移する' do
+        signup_link = find_all('a')[5].native.inner_text
+        click_link signup_link
+        expect(page).to have_content 'User info'
+      end
+    end
+
+    context 'ログインしていない場合の挙動を確認' do
+      it 'Log inリンクをクリックしたらログイン画面へ遷移する' do
+        login_link = find_all('a')[4].native.inner_text
+        click_link login_link
+        expect(current_path).to eq(new_user_session_path)
+      end
+      it 'Sign upリンクをクリックしたらログイン画面へ遷移する' do
+        signup_link = find_all('a')[5].native.inner_text
+        click_link signup_link
+        expect(current_path).to eq(new_user_registration_path)
+      end
+    end
+  end
+end
